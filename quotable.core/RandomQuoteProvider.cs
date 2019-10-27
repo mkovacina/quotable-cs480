610e49744aa59eda3cee43c75cdb04cb146624e8
Mike Kovacina <michael.kovacina@gmail.com>
2019-10-27 10:05:34 -0400

    initial pass at the RandomQuoteProvider interface

---
+++ quotable.core/RandomQuoteProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quotable.core
+{
+	public interface RandomQuoteProvider
+	{
+		IEnumerable<string> GetRandomQuotes(long numberOfQuotes = 0);
+	}
+}
